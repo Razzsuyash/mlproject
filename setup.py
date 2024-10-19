dc29c8f2669618abe54dcb836981008717de88a8
Razzsuyash <suyashraj2000@gmail.com>
2024-10-19 19:58:04 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
-HYPHEN_E_DOT ='-e .'
-def get_requirement(file_path:str)->List[str]:
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirement(file_path: str) -> List[str]:
     '''
-    this function will return the list of requirements
+    This function will return the list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements= [req.replace("\n","") for req in requirements]
+        requirements = [req.replace("\n", "") for req in requirements]
         
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-    return requirements         
+    return requirements
+
 setup(
-    name= 'mlproject',
-    version= '0.0.1',
-    author= 'Suyash',
-    author_email= 'suyashraj2000@gmail.com',
-    packages= find_packages(),
+    name='mlproject',
+    version='0.0.1',
+    author='Suyash',
+    author_email='suyashraj2000@gmail.com',
+    packages=find_packages(),
     install_requires=get_requirement('requirements.txt')
 )
